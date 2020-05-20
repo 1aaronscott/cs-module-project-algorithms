337555926b1f6345dcaefc20256e0c4f2a646a22
1aaronscott <1aaronscott@users.noreply.github.com>
2020-05-20 16:26:05 -0500

    numbers product

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,31 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+import math
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
-
-    pass
+    mult_list = []
+#    print("length of original list: ", len(arr))
+#    print(arr, "\n", arr[:-1])
+    for i in range(len(arr)):
+        if i == 0:
+            mult_list.append(math.prod(arr[1:]))
+        elif i == (len(arr)-1):
+            mult_list.append(math.prod(arr[:-1]))
+        else:
+            mult_list.append(math.prod(arr[:i]+arr[i+1:]))
+    return mult_list
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+    #        9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    # arr = [7, 9, 1, 8, 6, 7, 8, 8, 7, 10]
+    # arr = [7, 9, 1, 8, 6, 0, 7, 8, 8, 7, 10]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
